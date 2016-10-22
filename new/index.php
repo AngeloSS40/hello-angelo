e25da9f34f3758437a392c9857813b8ad4139f57
Angelo Sison <angelo.sison@cobankiat.com.ph>
2016-10-22 09:56:23 +0800

    changing everything in my life
    i want to be a like my Lord Jesus

---
+++ new/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Jquery Draggable testing</title>
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="plugins/font-awesome-4.6.3/css/font-awesome.css">
+<link rel="stylesheet" href="plugins/jquery-ui-1.11.4.custom/jquery-ui.css">
+<script type="text/javascript" src="js/jquery-1.12.2.min.js"></script>
+<script type="text/javascript" src="plugins/jquery-ui-1.11.4.custom/jquery-ui.js"></script>
+<script type="text/javascript" src="js/bootbox.min.js"></script>
+</head>
+<body>
+<aside style="background-color:#999;width:50pt;height:400pt;">
+testing
+</aside>
+</body>
+</html>
